7dfc2c63798e54d9686df954a98f38301a80d936
TanizakiKosuke <tanizaki@stu.kobe-u.ac.jp>
2023-07-24 14:54:25 +0900

    つぶやきサービス

---
+++ src/main/java/jp/kobe_u/cs/daikibo/tsubuyaki/entity/SearchForm.java
@@ -0,0 +1,14 @@
+package jp.kobe_u.cs.daikibo.tsubuyaki.entity;
+
+public class SearchForm {
+    private String searchWord;
+
+    // ゲッターとセッター
+    public String getSearchWord() {
+        return searchWord;
+    }
+
+    public void setSearchWord(String searchWord) {
+        this.searchWord = searchWord;
+    }
+}
